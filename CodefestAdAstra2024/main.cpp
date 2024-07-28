d2b49ae97826e9904d22aed0a0f397f51c58ef56
Sebastian Urrea <sebastian.urrea@caseware.com>
2024-07-28 11:44:27 -0500

    Initial Commit

---
+++ CodefestAdAstra2024/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CodefestAdAstra2024
+//
+//  Created by Sebastian Urrea on 28/07/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
